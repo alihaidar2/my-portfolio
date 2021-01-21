6df4d31a08baaf2293e7b9c5b36ec92987fec555
Ali Haidar <ahaid090@uottawa.ca>
2021-01-21 13:37:14 -0500

    centered text and divs

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -4,10 +4,10 @@ import '../styles/Quote.css'
 function Quote() {
     return (
         <div className="quote-box">
-            <p className="quote"> 
+            <h5 className="quote"> 
             “Only one who devotes himself to a cause with his whole strength and soul can be a true master. 
             <br /> For this reason mastery demands all of a person.” 
-            <br /> – Albert Einstein </p>
+            <br /> – Albert Einstein </h5>
         </div>
         
     )
